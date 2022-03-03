becf7d7ab05861f9b9d6ccd4e132f17c9779c8ff
Sahil Aggarwal <84344454+49sahil@users.noreply.github.com>
2022-03-03 22:48:52 +0530

    Update taxslab.js

--- taxslab.js
+++ taxslab.js
@@ -1,25 +1,25 @@
-let amount= 6000000;
+let am= 600000;
 
 function taxc(amount){
     let tax=0;
     if(250000<amount<500000){
         tax+= (amount-250000)*0.05;
     }
-    if(500000<amount<750000){
-        tax+= (amount-500000)*0.1;
+    if(500000<amount && amount<750000){
+        tax= (amount-500000)*0.1 + 250000*0.05; 
     }
-    if(750000<amount<1000000){
-        tax+= (amount-750000)*0.15;
+    if(750000<amount && amount<1000000){
+        tax+= (amount-750000)*0.15 + 250000*0.1 +250000*0.05;
     }
-    if(1000000<amount<1250000){
-        tax+= (amount-1000000)*0.20;
+    if(1000000<amount && amount<1250000){
+        tax+= (amount-1000000)*0.20 + 250000*0.15 + 250000*0.1 +250000*0.05;
     }
-    if(1250000<amount<1500000){
-        tax+= (amount-1250000)*0.25;
+    if(1250000<amount && amount<1500000){
+        tax+= (amount-1250000)*0.25 + 250000*0.20 + 250000*0.15 + 250000*0.1 +250000*0.05;
     }
     if(1500000<amount){
-        tax+= (amount-1500000)*0.30;
+        tax+= (amount-1500000)*0.30 + 250000*0.25 + 250000*0.20 + 250000*0.15 + 250000*0.1 +250000*0.05 ;
     }
     console.log(tax);
 }
-taxc(amount);
+taxc(am);
